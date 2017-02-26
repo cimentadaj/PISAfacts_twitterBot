8270eaabd2430eb9d628b10845bb2211423a247d
cimentadaj <cimentadaj@gmail.com>
2017-02-26 20:40:23 +0100

    Added preamble with vectors to change

--- PISA_graph.R
+++ PISA_graph.R
@@ -22,55 +22,76 @@ variable_labeller <- function(variable) {
   }
 }
 
-pisa_2015 <- read_spss("/Users/cimentadaj/Downloads/PISA/CY6_MS_CMB_STU_QQQ.sav")
+pisa_2015 <- read_spss("/Users/cimentadaj/Downloads/PISA/PISA2015/CY6_MS_CMB_STU_QQQ.sav")
 
-pisa2015 <- pisa_2015
-names(pisa2015) <- tolower(names(pisa2015))
-pisa2015$region <- countrycode(pisa2015$cnt, "iso3c", "continent")
+country_var <- "cnt" # country variable name in lower case
+country_types <- "iso3c" # type of country name
+missing_labels <- c("Valid Skip",
+                    "Not Reached",
+                    "Not Applicable",
+                    "Invalid",
+                    "No Response") # Labels I want to ignore
+
+int_data <- pisa_2015
+names(int_data) <- tolower(names(int_data))
+
+int_data$region <- countrycode(int_data[[country_var]], country_types, "continent")
 
 # Saving country names and their equivalency
-country_attributes <- attr(pisa2015$cnt, "labels")
+country_attributes <- attr(int_data[[country_var]], "labels")
 
 # Reversing the 3 CHR code to names so I can search for countries
 # in a lookup table
 country_names <- reverse_name(country_attributes)
 
 # Lookup 3 CHR code and change them for long country names
-pisa2015$cnt <- country_names[pisa2015$cnt]
-attr(pisa2015$cnt, "labels") <- country_attributes
-
-# Labels I don't want to use
-missing_labels <- c("Valid Skip",
-                    "Not Reached",
-                    "Not Applicable",
-                    "Invalid",
-                    "No Response")
+int_data[, country_var] <- country_names[int_data$cnt]
+attr(int_data[[country_var]], "labels") <- country_attributes
 
 # Get variables which have a 'labels' attribute
 # and have other labels besides the missing_labels
 # vector
 
-subset_vars <- which(map_lgl(pisa2015, function(x)
-  !is.null(attr(x, "labels")) && length(setdiff(names(attr(x, "labels")), missing_labels)) >= 1))
+subset_vars <- 
+  int_data %>%
+  map_lgl(function(x)
+    !is.null(attr(x, "labels")) &&
+    length(setdiff(names(attr(x, "labels")), missing_labels)) >= 1) %>%
+  which()
 
 # Sample 1 variable from the valid variables from subset_vars
 # Combine it with the country variable and turn it all into a data.frame
-valid_df <- as.data.frame(pisa2015[c("cnt", "region", sample(names(subset_vars), 1))])
+valid_df <- 
+  int_data %>%
+  select_("cnt", "region", sample(names(subset_vars), 1)) %>%
+  as.data.frame()
 
 random_countries <- unique(valid_df$cnt)
 
 # Labels of the random variable from valid_df free of the missing labels
-(test <- setdiff(names(attr(valid_df[, names(valid_df)[3], drop = T], "labels")), missing_labels))
+var_labels <- attr(valid_df[, names(valid_df)[3], drop = T], 'labels')
+test <-
+  var_labels %>%
+  names() %>%
+  setdiff(missing_labels)
 
 # While the length of the test vector is > 4, sample a new variable.
 # This is done because we don't want a lot of labels
 while (length(test) > 4) {
-  valid_df <- as.data.frame(pisa2015[c("cnt", "region", sample(names(subset_vars), 1))])
-  test <- setdiff(names(attr(valid_df[, names(valid_df)[3], drop = T], "labels")), missing_labels)
+  
+  valid_df <- 
+    int_data %>%
+    select_("cnt", "region", sample(names(subset_vars), 1)) %>%
+    as.data.frame()
+  
+  test <-
+    var_labels %>%
+    names() %>%
+    setdiff(missing_labels)
 }
 
 # Get the labels from the random variable
-(labels <- reverse_name(attr(valid_df[, names(valid_df)[3], drop = T], 'labels')))
+(labels <- reverse_name(var_labels))
 var_name <- names(valid_df)[3]
 
 try_df <-
